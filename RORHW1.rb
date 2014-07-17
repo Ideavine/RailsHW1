96afe587cb1b5f63c7a5f724e9c5355b08f810f6
Virginia Dooley <virginia@ideavine.org>
2014-07-17 14:20:24 -0400

    -a

--- RORHW1.rb
+++ RORHW1.rb
@@ -12,4 +12,21 @@ my_peony.color = pink
 my_peony.type = perennial
 my_peony.plant_seed
 
+class Shoe
+	attr_accessor :type
+	attr_accessor :color
+	attr_accessor :style
+	attr_accessor :size
+	def try_on
+		puts "a perfect fit"
+	end
+end
+
+my_sneakers = Shoe.new
+
+my_sneakers.type = running
+my_sneakers.color = multi
+my_sneakers.style = classic
+my_sneakers.size = eight
+my_sneakers.try_on
 
